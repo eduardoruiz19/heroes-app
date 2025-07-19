24a871e8bb5e57a9d6cfeddb3f56670813430f4e
Eduardo Ruiz <eduardoruiz19@hotmail.com>
2025-07-18 22:57:45 -0500

    prueba

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom';
 
+
+
 const CharactersByHero = ({ alter_ego, characters}) => {
     // if ( alter_ego === characters ) return (<></>);
     // return <p>{ characters }</p>
